87b72cffe24c72c0e514f9d86be2afdc977e3d88
KatSbnv <my-box21@mail.ru>
2017-03-24 15:30:56 +0400

    Initial commit

---
+++ src/Hello/World.java
@@ -0,0 +1,19 @@
+package Hello;
+
+//вывод на экран фразы "Здравствуй мир" 100 раз
+
+public class World
+{
+
+
+    public static void main(String[] args)
+    {
+       String s = "Hello World!";
+
+        for ( int i=0; i<100; i++)
+        {
+            System.out.println(s);
+        }
+
+    }
+}
